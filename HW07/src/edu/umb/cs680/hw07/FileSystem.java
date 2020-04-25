a0a67c5d89641b368a30843c432916ff131918aa
yash51 <yashmahant51@gmail.com>
2020-04-25 14:41:19 -0400

    HW07

---
+++ HW07/src/edu/umb/cs680/hw07/FileSystem.java
@@ -0,0 +1,25 @@
+package edu.umb.cs680.hw07;
+
+import java.util.LinkedList;
+
+public class FileSystem {
+	private FileSystem() {};
+	
+	private LinkedList<Directory> directoryRoot;
+	private static FileSystem instance = null;
+	
+	
+	public static FileSystem getFileSystem() {
+		if(instance==null) 
+			instance = new FileSystem ();
+		return instance;  
+	}
+	//for testing purpose
+	public void addRootDir(Directory dir) {
+		directoryRoot = new LinkedList<Directory>();
+		directoryRoot.add(dir);
+	}
+	public LinkedList<Directory> getRootDirs() {
+		return this.directoryRoot;
+	}
+}
